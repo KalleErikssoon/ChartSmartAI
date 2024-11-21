0265ed5d443ab126e52d60416ea7d61a7f0449d5
Isaac Lindegren Ternbom <isaacl@student.chalmers.se>
2024-11-21 12:50:14 +0100

    Add comments

--- ml_pipelines/macd_pipeline/main_script.py
+++ ml_pipelines/macd_pipeline/main_script.py
@@ -19,11 +19,10 @@ def runpipeline():
     labeler.label_data()
 
     #Step 4: Post CSV file to Django backend
-    # File path to the CSV file
     file_path = "ml_pipelines/macd_pipeline/macd_data.csv"
 
-    # API endpoint of your Django project
-    url = "http://127.0.0.1:8000/db_updates/"  # Replace with your actual endpoint
+    # API endpoint
+    url = "http://127.0.0.1:8000/db_updates/"
 
     # Prepare the file for upload
     with open(file_path, 'rb') as f:
--- stock_project/stock_app/urls.py
+++ stock_project/stock_app/urls.py
@@ -3,5 +3,5 @@ from stock_app import views, api_views
 # Add home to path
 urlpatterns = [
     path('', views.home, name='home'), # Render home html page
-    path('db_updates/', api_views.upload_csv, name='upload_csv')
+    path('db_updates/', api_views.upload_csv, name='upload_csv') # endpoint for uploading macd data
 ]
