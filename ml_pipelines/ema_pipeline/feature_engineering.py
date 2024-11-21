2e157957290008947b23aab29a1e130bbab044d6
Malte Bengtsson <bmalte@student.chalmers.se>
2024-11-21 10:34:15 +0100

    Add comments to the feature_engineering.py pipeline file

--- ml_pipelines/ema_pipeline/feature_engineering.py
+++ ml_pipelines/ema_pipeline/feature_engineering.py
@@ -1,8 +1,9 @@
+# Imports
 import numpy as np
 import os
 import pandas as pd
 
-#Import the csv file
+#Access the csv file via absolute path
 file_path = os.path.abspath("ml_pipelines/ema_pipeline/ema_data.csv")
 
 # Check to see if the ema_data.csv file exists 
@@ -15,10 +16,12 @@ else:
     raise FileNotFoundError(f"File not found: {file_path}")
 
 
-#calculate EMA value
+#calculate EMA value according to "https://dayanand-shah.medium.com/exponential-moving-average-and-implementation-with-python-1890d1b880e6"
 def exponential_moving_average(prices, period):
+    #Add array of zeroes
     ema = np.zeros(len(prices))
 
+    #Weighting factor, multiplier which determines relative importance of recent prices vs older prices 
     weighting_factor= 2/(period + 1)
                          
     sma = np.mean(prices[:period])
@@ -28,23 +31,25 @@ def exponential_moving_average(prices, period):
     for i in range(period, len(prices)):
         ema[i] = (prices[i] * weighting_factor) + (ema[i - 1] * (1 - weighting_factor))
 
-    # Set initial values before the period to NaN (not calculable)
+    # Set initial values before the period to NaN (not calculable), since they cant be calculated
     ema[:period - 1] = np.nan
     return ema
     
-
+# Amount of datapoints, for example 10 days (or hours, depending)
 period = 10
+
 ema_values = exponential_moving_average(ema_data['close'].values, period)
 
+#Create ema column with the ema values
 ema_data['ema'] = ema_values
-
+#Add the ema column to the csv file
 ema_data.to_csv(file_path, index=False)
 
+#Print statements for testing
 print(f"Updated CSV saved with EMA values at {file_path}")
 print("Updated data preview:")
 print(ema_data[ema_data['symbol'] == 'GOOG'].head())
     
-
-
+#Print statements for testing and comparisons
 ema_data['pandas_ema'] = ema_data['close'].ewm(span=period, adjust=False).mean()
 print(ema_data[['ema', 'pandas_ema']].head(15))
