c2afb575368d83455d99c366c69981bbaa6e381c
jessie <luhui930@126.com>
2024-11-23 19:09:43 +0100

    #4  add rsi url path

--- stock_project/stock_app/urls.py
+++ stock_project/stock_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'), # Render home html page
     path('db_updates/', api_views.upload_csv, name='upload_csv'), # endpoint for uploading macd data
     path('db_updates/ema/', api_views.upload_ema, name='upload_ema'),
+    path('db_updates/rsi/', api_views.upload_rsi, name='upload_rsi'), 
     path('get_database/macd/', api_views.get_macd_data, name='get_macd'), 
+    
 ]
