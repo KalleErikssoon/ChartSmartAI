2baf2588dfefe2cd95742d9c030d85aabc855a88
Malte Bengtsson <malte.bengtsson@hotmail.se>
2024-11-24 22:24:57 +0100

    Add train model and evaluate methods.

--- ml_pipelines/model_implementation/model_trainer.py
+++ ml_pipelines/model_implementation/model_trainer.py
@@ -16,21 +16,46 @@ class ModelTrainer:
     def load_data(self):
         X_train = pd.read_csv(f"{self.train_data_path}/X_train.csv")
         x_test = pd.read_csv(f"{self.test_data_path}/X_test.csv")
-        Y_train = pd.read_csv(f"{self.train_data_path}/Y_train.csv")
-        Y_test = pd.read_csv(f"{self.test_data_path}/Y_test.csv")
+        Y_train = pd.read_csv(f"{self.train_data_path}/Y_train.csv").values.flatten()
+        Y_test = pd.read_csv(f"{self.test_data_path}/Y_test.csv").values.flatten()
 
         return X_train, x_test, Y_train, Y_test
 
 #Train the model
     def train_model(self, X_train, y_train):
-        #Initial weights set to 0
+        #Initial weights set to 0 including the intercept term
         initial_w = np.zeros(X_train.shape[1] + 1)
 
         #Optimize the cost function
         res = minimize(
             fun = lambda w: LogisticRegressionUtils.costFunction(w, X_train.values, y_train),
             x0 = initial_w,
-            
+            jac = True,
+            method = 'TNC'
         )
+
+        if res.success:
+            print("Optimization completed succesfully.")
+            self.weights = res.x
+        else:
+            raise ValueError("Optimization failed")
+        
+    def evaluate(self, X_test, y_test):
+        #Add intercept term to test set
+        X_test = np.concatenate([np.ones((X_test.shape[0], 1)), X_test.values], axis=1)
+
+        #Make predictions
+        probabilities = LogisticRegressionUtils.sigmoid(X_test @ self.weights)
+        #Convert probabilities to binary predictions
+        predictions = (probabilities >= 0.5).astype(int) 
+
+        # Print evaluation metrics
+        print("Classification Report:")
+        print(classification_report(y_test, predictions))
+        print(f"Accuracy: {accuracy_score(y_test, predictions):.4f}")
+        print(f"Precision: {precision_score(y_test, predictions, average='weighted'):.4f}")
+        print(f"Recall: {recall_score(y_test, predictions, average='weighted'):.4f}")
+
+    
 
 #Save the model ( and other data that might be of interest such as calculated weights)
