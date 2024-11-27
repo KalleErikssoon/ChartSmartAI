04bf64bccb17a45081117f12678c575e15c99c23
Indomet <mehmetasim35@hotmail.com>
2024-11-27 14:26:44 +0100

    add admin page

--- stock_project/stock_app/urls.py
+++ stock_project/stock_app/urls.py
@@ -3,10 +3,12 @@ from stock_app import views, api_views
 # Add home to path
 urlpatterns = [
     path('', views.home, name='home'), # Render home html page
-    path('db_updates/', api_views.upload_csv, name='upload_csv') # endpoint for uploading macd data
-]
-
-urlpatterns = [
-    path('', views.home, name='home'), # Render home html page
+    path('stockadmin/', views.admin, name='admin'), # Render admin html page
+    path('db_updates/', api_views.upload_csv, name='upload_csv'), # endpoint for uploading macd data
     path('db_updates/ema/', api_views.upload_ema, name='upload_ema')
 ]
+
+# urlpatterns = [
+#     path('', views.home, name='home'), # Render home html page
+#     path('db_updates/ema/', api_views.upload_ema, name='upload_ema')
+# ]
